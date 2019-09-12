dc21fc37991e89a63de4428ba69cde9ad79c4be1
David <david.yu@readify.net>
2019-09-12 14:10:47 +0800

    Add cypress key

--- cypress/integration/client_path.spec.js
+++ cypress/integration/client_path.spec.js
@@ -1,13 +1,15 @@
 /// <reference types="Cypress" />
 
 describe("client path", () => {
-  cy.visit("http://localhost")
-  cy.contains("I need some help with software").click()
-  cy.contains("Services")
-  cy.contains("Requirements")
-  cy.contains("Application")
-  cy.contains("Approved")
-  cy.contains("Meetings")
-  cy.contains("Execution of Service")
-  cy.contains("Handover")
+  it("should work", () => {
+    cy.visit("http://localhost:8000")
+    cy.contains("I need some help with software").click()
+    cy.contains("Services")
+    cy.contains("Requirements")
+    cy.contains("Application")
+    cy.contains("Approved")
+    cy.contains("Meetings")
+    cy.contains("Execution of Service")
+    cy.contains("Handover")
+  })
 })
